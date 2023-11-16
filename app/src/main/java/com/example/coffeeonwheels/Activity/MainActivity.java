a032a57b2848342f1275b4d3ac58644f7f073eb9
Saktimayee Nayak <131857334+Sakti2345@users.noreply.github.com>
2023-11-16 19:12:08 +0530

    Added THe Main Activity class
    All THe Fucntionality Done Now You Can Go For THe Domain classes

---
+++ app/src/main/java/com/example/coffeeonwheels/Activity/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.coffeeonwheels.Activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coffeeonwheels.Adapter.CategoryAdaptor;
+import com.example.coffeeonwheels.Adapter.PopularAdaptor;
+import com.example.coffeeonwheels.Domain.CategoryDomain;
+import com.example.coffeeonwheels.Domain.foodDomain;
+import com.example.coffeeonwheels.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    private RecyclerView.Adapter adapter,adapter2;
+    private RecyclerView recyclerViewCategoryList,recyclerViewPopularList;
+
+
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String user=getIntent().getStringExtra("username");
+        text=findViewById(R.id.name);
+        text.setText("Hi "+user);
+
+
+        recyclerViewCategory();
+        recyclerViewPopular();
+        bottomNavigation();
+    }
+    private void bottomNavigation(){
+        FloatingActionButton floatingActionButton=findViewById(R.id.cartBtn);
+        LinearLayout homeBtn=findViewById(R.id.homeBtn);
+        ConstraintLayout orderBtn=findViewById(R.id.orderBtn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CartListActivity.class));
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+            }
+        });
+
+        orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CartListActivity.class));
+            }
+        });
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Pizza","cat_1"));
+        category.add(new CategoryDomain("Burger","cat_2"));
+        category.add(new CategoryDomain("Hotdog","cat_3"));
+        category.add(new CategoryDomain("Drink","cat_4"));
+        category.add(new CategoryDomain("Donut","cat_5"));
+
+        adapter=new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
+    }
+    private void recyclerViewPopular(){
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewPopularList=findViewById(R.id.recyclerView2);
+        recyclerViewPopularList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<foodDomain> foodList=new ArrayList<foodDomain>();
+        foodList.add(new foodDomain("Pepperoni pizza","pop_1","slices pepperoni, mozzerella chese, fresh oregano, ground black pepper, pizza sauce",299));
+        foodList.add(new foodDomain("Cheese Burger","pop_2","beef, Gouda Cheese, Special Sauce, Lettuce, tomato",179));
+        foodList.add(new foodDomain("Vegetable pizza","pop_3","Olive oil, pitted kalamata, cherry tomatoes, fresh oregano, basil",199));
+        adapter2=new PopularAdaptor(foodList);
+        recyclerViewPopularList.setAdapter(adapter2);
+    }
+}
