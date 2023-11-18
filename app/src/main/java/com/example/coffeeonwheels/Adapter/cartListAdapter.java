40212e9d21b16ab2ac2a9b66f33207071f64eca6
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-18 20:17:59 +0530

    update The Functionality of adapter

--- app/src/main/java/com/example/coffeeonwheels/Adapter/cartListAdapter.java
+++ app/src/main/java/com/example/coffeeonwheels/Adapter/cartListAdapter.java
@@ -1,4 +1,103 @@
 package com.example.coffeeonwheels.Adapter;
 
-public class cartListAdapter {
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.coffeeonwheels.Domain.foodDomain;
+import com.example.coffeeonwheels.Helper.ManagementCart;
+import com.example.coffeeonwheels.Interface.changeNumberItemsListener;
+import com.example.coffeeonwheels.R;
+
+import java.util.ArrayList;
+
+public class cartListAdapter extends RecyclerView.Adapter<cartListAdapter.ViewHolder> {
+    private ArrayList<foodDomain> foodDomains;
+    private ManagementCart managementCart;
+    private changeNumberItemsListener changeNumberItemsListener;
+
+    public cartListAdapter(ArrayList<foodDomain> foodDomains, Context context, changeNumberItemsListener changeNumberItemsListener) {
+        this.managementCart=new ManagementCart(context);
+        this.changeNumberItemsListener=changeNumberItemsListener;
+        this.foodDomains = foodDomains;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate =LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_cart,parent,false);
+
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        holder.title.setText(foodDomains.get(position).getTitle());
+        holder.feeEachItem.setText(String.valueOf(foodDomains.get(position).getFee()));
+        holder.totalEachItem.setText(String.valueOf(Math.round(((foodDomains.get(position).getNumberInCart())*(foodDomains.get(position).getFee()))*100)/100));
+        holder.num.setText(String.valueOf(foodDomains.get(position).getNumberInCart()));
+
+        int drawableResourceId=holder.itemView.getContext().getResources().getIdentifier(foodDomains.get(position).getPic(),"drawable",holder.itemView.getContext().getPackageName());
+        Glide.with(holder.itemView.getContext())
+                .load(drawableResourceId)
+                .into(holder.pic);
+
+        holder.plusItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                managementCart.plusNumberFood(foodDomains, position, new changeNumberItemsListener() {
+                    @Override
+                    public void changed() {
+                        notifyDataSetChanged();
+                        changeNumberItemsListener.changed();
+                    }
+                });
+            }
+        });
+
+        holder.minusItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                managementCart.minusNumberFood(foodDomains, position, new changeNumberItemsListener() {
+                    @Override
+                    public void changed() {
+                        notifyDataSetChanged();
+                        changeNumberItemsListener.changed();
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodDomains.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title,feeEachItem;
+        ImageView pic,plusItem,minusItem;
+        TextView totalEachItem,num;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.titleTxt);
+            feeEachItem=itemView.findViewById(R.id.feeEachItem);
+            pic=itemView.findViewById(R.id.picCart);
+            totalEachItem=itemView.findViewById(R.id.totalEachItem);
+            num=itemView.findViewById(R.id.numberItemTxt);
+            plusItem=itemView.findViewById(R.id.plusCartBtn);
+            minusItem=itemView.findViewById(R.id.minusCartBtn);
+        }
+    }
 }
--- app/src/main/java/com/example/coffeeonwheels/Domain/CategoryDomain.java
+++ app/src/main/java/com/example/coffeeonwheels/Domain/CategoryDomain.java
@@ -1,4 +1,30 @@
 package com.example.coffeeonwheels.Domain;
 
+
 public class CategoryDomain {
+    private String title;
+    private String pic;
+
+    public CategoryDomain(String title, String pic) {
+        this.title = title;
+        this.pic=pic;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
 }
